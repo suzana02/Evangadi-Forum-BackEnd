ac37298a53edb1af989c286c241f2522723cf5cb
suzana02 <163354165+suzana02@users.noreply.github.com>
2024-08-12 20:27:09 -0400

    committed

--- db/config.js
+++ db/config.js
@@ -7,6 +7,7 @@ const dbConnection = mysql2.createPool({
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_DATABASE,
+	port:process.env.DB_PORT,
 	connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT, 10),
 });
 
